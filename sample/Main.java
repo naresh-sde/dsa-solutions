dc562c65b4fda9decf8b0a66b76cac91b629198c
Nagula Naresh <nareshn.sde@gmail.com>
2026-05-27 14:44:36 +0530

    start: first commit - sample

--- sample/Main.java
+++ sample/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("DSA Journey starts June 6, 2025 🚀");
+        System.out.println("DSA Journey starts June 6, 2025");
         System.out.println("One problem a day. Every day.");
     }
 }
